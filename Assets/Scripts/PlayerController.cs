8bbeecbe7cabd6cd1bc515531ceafa4544e5900c
Brandon <88041524+pandax000@users.noreply.github.com>
2022-08-18 21:58:19 -0400

    Cleaned up code
    Cleaned up messy PlayerController script and PlayerInputActions.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
     // Physics
@@ -7,40 +6,49 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public float jumpForce;
     public float gravity;
-    bool isFalling; // Float fix
     // Ground
     bool isGrounded;
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask whatIsGround;
-    // Controls
+    // Input
+    PlayerInputActions playerInput;
     float inputX;
     // Animation
     Animator anim;
 
     void Awake() {
-        rb = GetComponent<Rigidbody2D>(); // Set rb to Player's Rigidbody2D
-        anim = GetComponent<Animator>(); // Set anim to Player's Animator
+        // Get/Create components
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        playerInput = new PlayerInputActions();
+        // Input
+        playerInput.Player.Jump.performed += context => Jump();
+        playerInput.Player.Jump.canceled += context => StopJump();
+        playerInput.Player.Movement.performed += context => Movement(context.ReadValue<float>());
+        playerInput.Player.Movement.canceled += context => StopMovement();
+    }
+
+    void OnEnable() {
+        playerInput.Enable();
     }
 
     void Update() {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround); // Check if Player is grounded
-
-        // Check if player is running (for animation)
+        // Is player grounded?
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        // Is player running?
         if (inputX == 0) {
             anim.SetBool("isRunning", false);
         } else {
             anim.SetBool("isRunning", true);
         }
-
-        // Check if player is jumping (for animation)
+        // Is player jumping?
         if (isGrounded == false) {
             anim.SetBool("isJumping", true);
         } else {
             anim.SetBool("isJumping", false);
         }
-        
-        // Flip sprite when facing left
+        // Flip sprite
         if (inputX > 0) {
             transform.eulerAngles = new Vector3(0, 0, 0);
         } else if (inputX < 0) {
@@ -49,29 +57,31 @@ public class PlayerController : MonoBehaviour {
     }
     
     void FixedUpdate() {
-        // Check if Player is falling
-        if (rb.velocity.y < 0) {
-            isFalling = true;
-        } else if (rb.velocity.y >= 0) {
-            isFalling = false;
-        }
-        
-        rb.velocity = new Vector2(inputX * speed, rb.velocity.y); // Horizontal movement
+        // Movement
+        rb.velocity = new Vector2(inputX * speed, rb.velocity.y);
+    }
+
+    void OnDisable() {
+        playerInput.Disable();
     }
     
     // Custom methods
-    public void Jump(InputAction.CallbackContext context) {
-        // Jump
-        if (context.performed == true && isGrounded == true) {
-            anim.SetTrigger("takeOff"); // Trigger jump animation
+    void Jump() {
+        if (isGrounded == true) {
+            anim.SetTrigger("takeOff");
             rb.velocity = Vector2.up * jumpForce;
-        } else if (context.canceled == true && isFalling == false) {
-            rb.velocity = new Vector2(rb.velocity.x, gravity);
         }
     }
 
-    public void Movement(InputAction.CallbackContext context) {
-        // Horizontal movement
-        inputX = context.ReadValue<Vector2>().x;
+    void StopJump() {
+        rb.velocity = new Vector2(rb.velocity.x, gravity);
+    }
+
+    void Movement(float direction) {
+        inputX = direction;
+    }
+
+    void StopMovement() {
+        inputX = 0;
     }
 }
